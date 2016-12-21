92d5b2ff3f0f852899501d4b492c44c383579a55
Ahmad Agung <ahmad.agung.a@gmail.com>
2016-12-22 01:11:14 +0700

    Add build conf

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+// app.use('/css', express.static(__dirname + '/css'));
+// app.use('/img', express.static(__dirname + '/img'));
+// app.use('/static', express.static(__dirname + '/static'));
+app.use(express.static('./build'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './build', 'index.html'));
+});
+
+app.listen(9000);
+console.log('listen to 9000');

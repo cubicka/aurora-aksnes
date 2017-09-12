5812c09e75046a419a203fe729dcdafe69162531
Ahmad Agung <ahmad.agung.a@gmail.com>
2017-09-12 19:30:17 +0700

    For rulo

--- src/component/etalaseItem.js
+++ src/component/etalaseItem.js
@@ -55,8 +55,8 @@ const EtalaseItem = React.createClass({
         this.props.UpdateQuantity(q || 0);
     },
     render() {
-        const {name: nama, price: harga, ukuran, image, count} = this.props;
-        console.log('p', this.props)
+        const {name: nama, harga, ukuran, image, count} = this.props;
+        // console.log('p', this.props)
         const namakelas = count > 0 ? style.item + " " + style.selected : style.item;
 
         return (
